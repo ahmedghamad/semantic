f29f3b015dc3cbc29d341f0b26f22920e1dfe2ec
Ahmed Hamad <manchester-88@hotmail.com>
2023-01-14 17:47:24 +0000

    Spacey similiarty task

--- semantic.py
+++ semantic.py
@@ -5,11 +5,12 @@ nlp =spacy.load ('en_core_web_md')
 word1 =nlp("cat")
 word2 = nlp ("monkey")
 word3 = nlp ("banana")
-word4 = nlp ("human")
 print (word1.text, word2.text, word1.similarity(word2))
 print (word3.text, word2.text, word3.similarity(word2))
 print (word3.text, word1.text, word3.similarity(word1))
 
+""" its amazing to see that cat a moneky have the highies  similiraty because both are animails, monkey and banana have a higher similiraty than cat and banana. 
+Also I have added human and car but in my opnion human should have higer similiraty with all these as human do eat apples, bananas, have cats as pets, drive cars and like monkeys"""
 tokens =nlp ("human cat apple monkey banana car")
 for token1 in tokens:
     for token2 in tokens:
@@ -21,3 +22,5 @@ model_sentence = nlp (sentence_to_compare)
 for sentence in sentences:
     similarity = nlp (sentence).similarity (model_sentence)
     print (sentence + " - ", similarity)
+
+# when I have ran the example file with the 'en_core_web_sm' all the similiraty figure were much lower than using the 'en_core_web_md'
